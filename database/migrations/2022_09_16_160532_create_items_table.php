054b0bf295ae204623f433d6e6eb772ae853e134
Taufiqur Rahman <62732164+taufiqurSabbir@users.noreply.github.com>
2023-09-16 22:27:14 +0600

    migration

--- database/migrations/2022_09_16_160532_create_items_table.php
+++ database/migrations/2022_09_16_160532_create_items_table.php
@@ -13,6 +13,8 @@ return new class extends Migration
     {
         Schema::create('items', function (Blueprint $table) {
             $table->id();
+
+            $table->string('price');
             $table->timestamps();
         });
     }
--- database/migrations/2023_09_16_160512_create_search_histories_table.php
+++ database/migrations/2023_09_16_160512_create_search_histories_table.php
@@ -13,6 +13,8 @@ return new class extends Migration
     {
         Schema::create('search_histories', function (Blueprint $table) {
             $table->id();
+            $table->bigInteger('user_id');
+            $table->foreignId('item_id')->constrained('items');
             $table->timestamps();
         });
     }
